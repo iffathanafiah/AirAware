deaaa0006c79196a49eca53eee25c21b9574777e
Iffat Hanafiah <muhammadiffath@gmail.com>
2024-06-03 12:20:58 +0800

    Create test.php

---
+++ test.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>test</title>
+    </head>
+    <body>
+        <h1>test</h1>
+        <?php include('./include/MalaysiaAQI.html');?>
+    </body>
+</html> 
