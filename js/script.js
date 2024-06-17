f7515c12f439423ab72a64e5616b50ce89b898c4
Iffat Hanafiah <muhammadiffath@gmail.com>
2024-06-17 17:13:45 +0800

    finishing AQIData.html and creating two new pages

--- js/script.js
+++ js/script.js
@@ -133,8 +133,8 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
   }
   
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
+ // Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
       var dropdowns = document.getElementsByClassName("dropdown-content");
       var i;
